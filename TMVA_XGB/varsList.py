29ae29cccba37e51a0166148cbe385fcf9306a64
Jingyu Luo <jluo48@pbrux40cit.hep.brown.edu>
2025-08-21 22:10:14 -0400

    update XGB scripts

--- TMVA_XGB/varsList.py
+++ TMVA_XGB/varsList.py
@@ -3,8 +3,9 @@
 #input variables
 varList = {}
 
-bkginputDir = '/isilon/hadoop/store/user/dali/FWLJMET106XUL_singleLep2017UL_RunIISummer20_3t_step2/nominal/' 
-siginputDir = '/isilon/hadoop/store/group/bruxljmFWLJMET106XUL_singleLep2017UL_RunIISummer20_3t_step2/nominal/' 
+bkginputDir = '/isilon/hadoop/store/user/fsimpson/FWLJMET106XUL_singleLep2017UL_RunIISummer20v2_step2/nominal/' 
+siginputDir = '/isilon/hadoop/store/group/bruxljm/jingyu/FWLJMET106XUL_singleLep2017UL_RunIISummer20v2PUupdated__step2/nominal/'
+step3Dir = '/isilon/hadoop/store/group/bruxljm/jluo48/CHiggs_XGB/XGB_NEW_reduced/UL17/nominal/' 
 
 sig_List = {
 200:'ChargedHiggs_HplusTB_HplusToTB_M-200_TuneCP5_13TeV_amcatnlo_pythia8_hadd.root',
@@ -403,6 +404,103 @@ varList['NewVar_added'] = [
  
 ]
 
+varList['NewVar_reduced'] = [
+['AK4HTpMETpLepPt','S_{T}','GeV'],
+#['minMleppBjet','min[M(l,b)]','GeV'],
+['mass_minBBdr','M(b,b) with min[#DeltaR(b,b)]','GeV'],
+['deltaR_lepBJet_maxpt','#DeltaR(l,b)] with max[p_{T}(l,b)]',''],
+#['lepDR_minBBdr','#DeltaR(l,bb) with min[#DeltaR(b,b)]',''],
+##['centrality','Centrality',''],
+#['deltaEta_maxBB','max[#Delta#eta(b,b)]',''],
+#['aveCSVpt','p_{T} weighted CSVv2',''],
+##['aveBBdr','ave[#DeltaR(b,b)]',''],
+##['FW_momentum_0','0^{th} FW moment','GeV'],
+##['FW_momentum_1','1^{st} FW moment','GeV'],
+['FW_momentum_2','2^{nd} FW moment','GeV'],
+##['FW_momentum_3','3^{rd} FW moment','GeV'],
+##['FW_momentum_4','4^{th} FW moment','GeV'],
+##['FW_momentum_5','5^{th} FW moment','GeV'],
+##['FW_momentum_6','6^{th} FW moment','GeV'],
+#['mass_maxJJJpt','M(jjj) with max[p_{T}(jjj)]','GeV'],
+#['BJetLeadPt','p_{T}(b_{1})','GeV'],
+##['deltaR_minBB','min[#DeltaR(b,b)]',''],
+##['minDR_lepBJet','min[#DeltaR(l,b)]',''],
+['MT_lepMet','M_{T}(lep,E_{T}^{miss})','GeV'],
+['AK4HT','H_{T}','GeV'],
+##['hemiout','Hemiout','GeV'],
+['theJetLeadPt','p_{T}(j_{1})','GeV'],
+['corr_met','E_{T}^{miss}','GeV'],
+##['leptonPt_MultiLepCalc','p_{T}(lep)','GeV'],
+#['mass_lepJets0','M(l,j_{1})','GeV'], 
+#['mass_lepJets1','M(l,j_{2})','GeV'],
+##['mass_lepJets2','M(l,j_{3})','GeV'],
+#['MT2bb','MT2bb','GeV'],
+['mass_lepBJet0','M(l,b_{1})','GeV'],
+['mass_lepBJet_mindr','M(l,b) with min[#DeltaR(l,b)]','GeV'],
+##['secondJetPt','p_{T}(j_{2})','GeV'],
+##['fifthJetPt','p_{T}(j_{5})','GeV'],
+#['sixthJetPt','p_{T}(j_{6})','GeV'],
+#['PtFifthJet','5^{th} jet p_{T}','GeV'],
+##['mass_minLLdr','M(j,j) with min[#DeltaR(j,j)], j #neq b','GeV'],
+['mass_maxBBmass','max[M(b,b)]','GeV'],
+##['deltaR_lepJetInMinMljet','#DeltaR(l,j) with min M(l, j)',''],
+#['deltaPhi_lepJetInMinMljet','#DeltaPhi(l,j) with min M(l, j)',''],
+#['deltaR_lepbJetInMinMlb','#DeltaR(l,b) with min M(l, b)',''],
+##['deltaPhi_lepbJetInMinMlb','#DeltaPhi(l,b) with min M(l, b)',''],
+['M_allJet_W','M(allJets, leptoninc W)','GeV'],
+['HT_bjets','HT(bjets)','GeV'],
+##['ratio_HTdHT4leadjets','HT/HT(4 leading jets)',''],
+#['csvJet3','DeepCSV(3rdPtJet)',''],
+#['csvJet4','DeepCSV(4thPtJet)',''],
+['thirdcsvb_bb','DeepCSV(3rdDeepCSVJet)',''],
+['fourthcsvb_bb','DeepCSV(4thDeepCSVJet)',''],
+##['NJets_JetSubCalc','AK4 jet multiplicity',''],
+##['HT_2m','HTwoTwoPtBjets','GeV'],
+['Sphericity','Sphericity','Sphericity'],
+['Aplanarity','Aplanarity','Aplanarity'],
+#
+#['recLeptonicTopPt', 'recLeptonicTopPt', 'recLeptonicTopPt'],
+#['recLeptonicTopEta', 'recLeptonicTopEta', 'recLeptonicTopEta'],
+#['recLeptonicTopPhi', 'recLeptonicTopPhi', 'recLeptonicTopPhi'],
+#['recLeptonicTopMass', 'recLeptonicTopMass', 'recLeptonicTopMass'],
+#
+['BestTop_Discriminator', 'BestTop_Discriminator', 'BestTop_Discriminator'],
+#['BestTop_Pt', 'BestTop_Pt', 'BestTop_Pt'],
+#['BestTop_Phi', 'BestTop_Phi', 'BestTop_Phi'],
+#['BestTop_Eta', 'BestTop_Eta', 'BestTop_Eta'],
+#['BestTop_Mass', 'BestTop_Mass', 'BestTop_Mass'],
+#
+#
+['NoTop_Jet1_CSV', 'NoTop_Jet1_CSV', 'NoTop_Jet1_CSV'],
+['NoTop_Jet1_Pt', 'NoTop_Jet1_Pt', 'NoTop_Jet1_Pt'],
+#['NoTop_Jet1_Phi', 'NoTop_Jet1_Phi', 'NoTop_Jet1_Phi'],
+#['NoTop_Jet1_Eta', 'NoTop_Jet1_Eta', 'NoTop_Jet1_Eta'],
+#['NoTop_Jet1_Energy', 'NoTop_Jet1_Energy', 'NoTop_Jet1_Energy'],
+#
+['NoTop_Jet2_CSV', 'NoTop_Jet2_CSV', 'NoTop_Jet2_CSV'],
+#['NoTop_Jet2_Pt', 'NoTop_Jet2_Pt', 'NoTop_Jet2_Pt'],
+#['NoTop_Jet2_Phi', 'NoTop_Jet2_Phi', 'NoTop_Jet2_Phi'],
+#['NoTop_Jet2_Eta', 'NoTop_Jet2_Eta', 'NoTop_Jet2_Eta'],
+#['NoTop_Jet2_Energy', 'NoTop_Jet2_Energy', 'NoTop_Jet2_Energy'],
+#
+['recLeptonicTopJetCSV', 'recLeptonicTopJetCSV', 'recLeptonicTopJetCSV'],
+#['recLeptonicTopJetPt', 'recLeptonicTopJetPt', 'recLeptonicTopJetPt'],
+#
+['LeptonicTB1_M', 'LeptonicTB1_M', 'LeptonicTB1_M'],
+['LeptonicTB2_M', 'LeptonicTB2_M', 'LeptonicTB2_M'],
+#
+#['LeptonicTB1_Pt', 'LeptonicTB1_Pt', 'LeptonicTB1_Pt'],
+#['LeptonicTB2_Pt', 'LeptonicTB2_Pt', 'LeptonicTB2_Pt'],
+#
+#['HadronicTB1_M', 'HadronicTB1_M', 'HadronicTB1_M'],
+['HadronicTB2_M', 'HadronicTB2_M', 'HadronicTB2_M'],
+#
+#['HadronicTB1_Pt', 'HadronicTB1_Pt', 'HadronicTB1_Pt'],
+#['HadronicTB2_Pt', 'HadronicTB2_Pt', 'HadronicTB2_Pt']
+
+ 
+]
+
 varList['NewVar_nc'] = [
 ['AK4HTpMETpLepPt','S_{T}','GeV'],
 ['minMleppBjet','min[M(l,b)]','GeV'],
@@ -438,8 +536,8 @@ varList['NewVar_nc'] = [
 ['mass_lepBJet_mindr','M(l,b) with min[#DeltaR(l,b)]','GeV'],
 #['secondJetPt','p_{T}(j_{2})','GeV'],
 #['fifthJetPt','p_{T}(j_{5})','GeV'],
-['sixthJetPt','p_{T}(j_{6})','GeV'],
-['PtFifthJet','5^{th} jet p_{T}','GeV'],
+#['sixthJetPt','p_{T}(j_{6})','GeV'],
+#['PtFifthJet','5^{th} jet p_{T}','GeV'],
 #['mass_minLLdr','M(j,j) with min[#DeltaR(j,j)], j #neq b','GeV'],
 ['mass_maxBBmass','max[M(b,b)]','GeV'],
 #['deltaR_lepJetInMinMljet','#DeltaR(l,j) with min M(l, j)',''],
